6541ac5d93fe36dd5038c2b53cd8ba3a5c160e94
0x1 <kevin.francart3@gmail.com>
2026-08-01 13:50:10 +0200

    fix(ffi): fix various FFI implementations

--- src/Record/Unsafe.go
+++ src/Record/Unsafe.go
@@ -3,29 +3,30 @@ package Record_Unsafe
 import "gopurs/output/gopurs_runtime"
 
 func UnsafeHas(label string, recVal interface{}) bool {
-	rec := gopurs_runtime.RecordToMap(recVal.(gopurs_runtime.Value))
-	_, ok := rec[label]
+	v := recVal.(gopurs_runtime.Value)
+	m := gopurs_runtime.RecordToMap(v)
+	_, ok := m[label]
 	return ok
 }
+
 func UnsafeGet(label string, recVal interface{}) interface{} {
-	rec := gopurs_runtime.RecordToMap(recVal.(gopurs_runtime.Value))
-	return rec[label]
+	v := recVal.(gopurs_runtime.Value)
+	return gopurs_runtime.RecordGet(v, label)
 }
+
 func UnsafeSet(label string, value interface{}, recVal interface{}) interface{} {
-	rec := gopurs_runtime.RecordToMap(recVal.(gopurs_runtime.Value))
-	newMap := make(map[string]gopurs_runtime.Value, len(rec)+1)
-	for k, v := range rec {
-		newMap[k] = v
-	}
-	newMap[label] = gopurs_runtime.Any(value)
-	return gopurs_runtime.Record(newMap)
+	v := recVal.(gopurs_runtime.Value)
+	val := value.(gopurs_runtime.Value)
+	return gopurs_runtime.RecordUpdate1(v, label, val)
 }
+
 func UnsafeDelete(label string, recVal interface{}) interface{} {
-	rec := gopurs_runtime.RecordToMap(recVal.(gopurs_runtime.Value))
+	v := recVal.(gopurs_runtime.Value)
+	m := gopurs_runtime.RecordToMap(v)
 	newMap := make(map[string]gopurs_runtime.Value)
-	for k, v := range rec {
+	for k, val := range m {
 		if k != label {
-			newMap[k] = v
+			newMap[k] = val
 		}
 	}
 	return gopurs_runtime.Record(newMap)
