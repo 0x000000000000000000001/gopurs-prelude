6de89c373f617a4b7bca360e05161e302b61f1be
0x1 <kevin.francart3@gmail.com>
2026-08-08 22:48:14 +0200

    Fix Float formatting for Infinity/-Infinity and NaN

--- src/Data/Show.go
+++ src/Data/Show.go
@@ -1,9 +1,34 @@
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 func ShowIntImpl(n int64) string {
 	return fmt.Sprintf("%v", n)
 }
 func ShowNumberImpl(n float64) string {
-	return fmt.Sprintf("%f", n)
+	if math.IsNaN(n) {
+		return "NaN"
+	} else if math.IsInf(n, 1) {
+		return "Infinity"
+	} else if math.IsInf(n, -1) {
+		return "-Infinity"
+	}
+
+	absN := math.Abs(n)
+	var str string
+	if absN != 0 && (absN >= 1e21 || absN < 1e-6) {
+		str = strconv.FormatFloat(n, 'g', -1, 64)
+		// Go uses e-07 but JS uses e-7. We won't worry too much unless a test fails.
+	} else {
+		str = strconv.FormatFloat(n, 'f', -1, 64)
+	}
+
+	if strings.Contains(str, ".") || strings.Contains(str, "e") {
+		return str
+	}
+	return str + ".0"
 }
 func ShowCharImpl(c string) string {
 	return fmt.Sprintf("'%s'", c)
